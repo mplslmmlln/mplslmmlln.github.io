4130d912c7d4e56f4de3cb187623ec464dbb6681
Dmitriy <70741044+mplslmmlln@users.noreply.github.com>
2020-10-14 22:47:40 +0300

    Генератор имён

---
+++ init.js
@@ -0,0 +1,26 @@
+window.onload = function()
+{
+	const initPerson = personGenerator.getPerson();
+	document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+	document.getElementById('surnameOutput').innerText = initPerson.Surname;
+	document.getElementById('genderOutput').innerText = initPerson.gender;
+	document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+};
+
+document.getElementById("butGen").addEventListener('click', function() 
+	{	const initPerson = personGenerator.getPerson();
+		document.getElementById('firstNameOutput').innerText = initPerson.firstName;
+		document.getElementById('surnameOutput').innerText = initPerson.Surname;
+		document.getElementById('genderOutput').innerText = initPerson.gender;
+		document.getElementById('birthYearOutput').innerText = initPerson.birthYear;
+
+	});
+
+document.getElementById("butRes").addEventListener('click', function() 
+{
+	const initPerson = personGenerator.getPerson();
+	document.getElementById('firstNameOutput').innerText = '_______';
+	document.getElementById('surnameOutput').innerText = '_______';
+	document.getElementById('genderOutput').innerText = '_______';
+	document.getElementById('birthYearOutput').innerText = '_______';
+});
