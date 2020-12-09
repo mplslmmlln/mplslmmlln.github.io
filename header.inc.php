396d760d184d5e13f4b33daac10ce760e7783b43
Dmitriy <70741044+mplslmmlln@users.noreply.github.com>
2020-12-09 22:57:05 +0300

    Delete header.inc.php

--- header.inc.php
+++
@@ -1,8 +0,0 @@
-<nav class="menu">
-  <ul>
-    <li><a href="#">Главная</a></li>
-    <li><a href="#">Проекты</a></li>
-    <li><a href="#">О нас</a></li>
-    <li><a href="#">Контакты</a></li>
-  </ul>
-</nav>
